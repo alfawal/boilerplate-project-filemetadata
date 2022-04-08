8a57462029395448c62697dd83d3e30e433887e0
Abdulrahman Alfawal <61620817+alfawal@users.noreply.github.com>
2022-04-08 21:26:23 +0000

    add: solution

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
 
 var app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile') ,function (req, res) {
+  const {originalname: name, mimetype: type, size} = req.file
+    return res.json({name, type, size})
 });
 
 
