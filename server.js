f8d490a6c49951a574ef19639ff45492ac33fe59
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2022-04-09 00:27:48 +0300

    format

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-const multer  = require('multer')
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
 
 var app = express();
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', upload.single('upfile') ,function (req, res) {
-  const {originalname: name, mimetype: type, size} = req.file
-    return res.json({name, type, size})
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  const { originalname: name, mimetype: type, size } = req.file;
+  return res.json({ name, type, size });
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
